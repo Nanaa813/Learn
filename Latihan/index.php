4f67ae2fea4b881dfc32df9745237ecb8d675be4
Nanaa813 <alee95514@gmail.com>
2024-10-13 12:45:35 +0800

    organize

---
+++ Latihan/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+    $text = "Hello, World!";
+    $color = "red"; // Change this to any color you want
+    echo "<span style='color: $color;'>$text</span>";
+?>
+</head>
+<body>
+    <?= "WELCOME TO NANA"; ?>
+</body>
+</html>
