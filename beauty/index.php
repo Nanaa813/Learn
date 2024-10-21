d846bcb53b06c344bd0ce39de8da38bcec4ec0dd
meri17283 <meririski17@gmail.com>
2024-10-21 16:49:31 +0800

    add index

---
+++ beauty/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Index | Beauty</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+</head>
+<body id="bg-index" style="	background-image: url(images/bgpink.png); background-repeat: no-repeat; background-position: center; display: fixed; background-size: cover;">>
+	<div class="container">
+    	<h1>Welcome</h1>
+    	<p>Please choose an option:</p>
+   		<form action="register.php" method="get">
+        	<button type="submit" class="register">Register</button>
+    	</form>
+    	<form action="login.php" method="get">
+        	<button type="submit" class="login">Login</button>
+    	</form>
+	</div>
+</body>
+</html>
